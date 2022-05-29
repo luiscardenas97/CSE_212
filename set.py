210920333238a782c6a11058fdbf93a848506efe
luiscardenas97 <97266653+luiscardenas97@users.noreply.github.com>
2022-05-28 20:49:42 -0600

    Luis

--- set.py
+++ set.py
@@ -1,19 +1,13 @@
-set1 = set()
-set2 = set()
-ser = [1,2,3,2,5,2,3,6,7,2,3,4,1,8]
-ser2 = [2,4,5,3,6,2,4,6,2,4,2,6,3,4]
-for x in ser:
-    set1.add(x)
 
-for x in ser2:
-    set2.add(x)
+sec = ["am", "at", "ma", "if", "fi","aa"]
+
+set4 = set()
+for x in sec:
+    if x in set4:
+        set4.add(x)
+print(set4)
 
 
-print(set1)
-print(set2)
-set3 = set()
-for x in set1:
-    if x in set2:
-        set3.add(x)
 
-print(set3)
+
+
